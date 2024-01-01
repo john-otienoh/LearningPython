84ae530c6b3b8d29b9905a752545837bf1b53727
john-otienoh <johnteclaire@gmail.com>
2024-01-01 22:37:34 +0300

    Day 1-band-name-generator project

---
+++ 100_DaysOfPython/README.md
@@ -0,0 +1,2 @@
+This Directory contain python programs from the Angela Yu Python Programming
+Let's see what we can do with python in 2024:[metal]: :[rocket]:
---
+++ 100_DaysOfPython/band_name_generator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")
